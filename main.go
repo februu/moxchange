e5f82926a58003011d5068f52604bbc9b5ee7899
febru <81876955+februu@users.noreply.github.com>
2026-03-20 20:24:05 +0100

    Add port and file flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
@@ -11,8 +12,12 @@ import (
 const VERSION = "x.x.x"
 
 func main() {
-	printBanner()
-	ds, error := datasource.NewCSVDataSource("./test.csv", true)
+	port := flag.Int("port", 3777, "Port to listen on")
+	file := flag.String("file", "./data.csv", "Csv file path")
+	flag.Parse()
+
+	printBanner(*port)
+	ds, error := datasource.NewCSVDataSource(*file, true)
 	if error != nil {
 		log.Println(error)
 		return
@@ -30,11 +35,10 @@ func main() {
 			return
 		}
 		fmt.Println(kline)
-
 	}
 }
 
-func printBanner() {
+func printBanner(port int) {
 	const cyan = "\033[36m"
 	const green = "\033[32m"
 	const reset = "\033[0m"
@@ -42,5 +46,5 @@ func printBanner() {
 	fmt.Printf("Version: %s%s%s | Created by %sfebruu%s\n", cyan, VERSION, reset, cyan, reset)
 	fmt.Printf("Repository: %shttps://github.com/februu/moxchange%s\n", cyan, reset)
 	fmt.Printf("Docs: %shttps://febru.dev/moxchange%s\n\n", cyan, reset)
-	fmt.Printf("⚡ Connect your client here: %s%s%s\n\n", green, "127.0.0.1:3777/ws", reset)
+	fmt.Printf("⚡ Connect your client here: %s127.0.0.1:%d/ws%s\n\n", green, port, reset)
 }
