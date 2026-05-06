bbe18c5209f79ab9327496fb8c03b10c790281c5
febru <81876955+februu@users.noreply.github.com>
2026-05-06 17:03:59 +0200

    Refactor DataFeed protocol

--- moxchange/feeds/data_feed.py
+++ moxchange/feeds/data_feed.py
@@ -3,4 +3,7 @@ from moxchange.types import Kline
 
 
 class DataFeed(Protocol):
+    """Protocol for data feeds that provide Kline data."""
+
+    def __next__(self) -> Kline: ...
     def __iter__(self) -> Iterator[Kline]: ...
